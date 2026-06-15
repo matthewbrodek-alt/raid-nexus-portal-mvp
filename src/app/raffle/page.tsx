9b1622463948197feafb3e417e0910be5b50d045
MatthewBrodek <matthewbrodek@gmail.com>
2026-06-16 00:51:42 +0700

    words

--- src/app/raffle/page.tsx
+++ src/app/raffle/page.tsx
@@ -234,7 +234,7 @@ export default function RafflePage() {
                   key={`${activeVideo}-${videoNonce}-${videoSourceIndex}`}
                   ref={videoRef}
                   src={activeVideoSource?.src}
-                  className="raffle-character-video pointer-events-none absolute left-1/2 top-1/2 z-[4] h-full w-full -translate-x-1/2 -translate-y-1/2 object-contain object-center opacity-100"
+                  className="raffle-character-video pointer-events-none absolute left-1/2 top-[30%] z-[4] h-full w-full -translate-x-1/2 -translate-y-1/2 object-contain object-center opacity-100"
                   muted
                   playsInline
                   preload="auto"
